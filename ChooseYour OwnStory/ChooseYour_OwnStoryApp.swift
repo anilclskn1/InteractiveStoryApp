57dab48abcbe7f635c415ef6e0ef9aabd7c182b2
Anil Caliskan <anilclskn.1@gmail.com>
2024-02-12 13:15:35 +0300

    Initial Commit

---
+++ ChooseYour OwnStory/ChooseYour_OwnStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChooseYour_OwnStoryApp.swift
+//  ChooseYour OwnStory
+//
+//  Created by Anil Caliskan on 12.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChooseYour_OwnStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
