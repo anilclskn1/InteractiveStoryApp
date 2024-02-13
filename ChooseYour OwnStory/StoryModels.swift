548ee66794cfa5c02441fac72aaa1a33aca8358e
Anil Caliskan <anilclskn.1@gmail.com>
2024-02-13 22:11:17 +0300

    first commit

---
+++ ChooseYour OwnStory/StoryModels.swift
@@ -0,0 +1,8 @@
+//
+//  StoryModels.swift
+//  ChooseYour OwnStory
+//
+//  Created by Anil Caliskan on 12.02.2024.
+//
+
+import Foundation
---
+++ ChooseYour OwnStory/StoryPageView.swift
@@ -0,0 +1,18 @@
+//
+//  StoryPageView.swift
+//  ChooseYour OwnStory
+//
+//  Created by Anil Caliskan on 12.02.2024.
+//
+
+import SwiftUI
+
+struct StoryPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoryPageView()
+}
