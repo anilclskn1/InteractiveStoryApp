58b1006f5523f6e5a3b33597023281b65f867d95
Anil Caliskan <anilclskn.1@gmail.com>
2024-02-13 22:11:34 +0300

    Interactive SwiftUI

--- ChooseYour OwnStory/StoryModels.swift
+++ ChooseYour OwnStory/StoryModels.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct Story {
+    let pages: [StoryPage]
+    
+    subscript(_ pageIndex: Int) -> StoryPage {
+        return pages[pageIndex]
+    }
+}
+
+struct StoryPage {
+    let text: String
+    
+    let choices: [Choice]
+    
+    init(text: String, choices: [Choice]) {
+        self.text = text
+        self.choices = choices
+    }
+}
+
+struct Choice {
+    let text: String
+    let destination: Int
+}
--- ChooseYour OwnStory/StoryPageView.swift
+++ ChooseYour OwnStory/StoryPageView.swift
@@ -8,11 +8,36 @@
 import SwiftUI
 
 struct StoryPageView: View {
+    
+    let story: Story
+    let pageIndex: Int
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ScrollView {
+                Text(story[pageIndex].text)
+            }
+            //SwiftUI uses the id argument to tell the choices apart from each other. Because each choice has different text, you can use the text property to identify them.
+            ForEach(story[pageIndex].choices, id: \Choice.text) { choice in
+                //A NavigationLink needs two things: a destination and content to display. The destination of the navigation link is the page that the choice navigates to.
+                NavigationLink(destination: StoryPageView(story: story, pageIndex: choice.destination)) {
+                    Text(choice.text)
+                        .multilineTextAlignment(.leading)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding()
+                        .background(Color.gray.opacity(0.25))
+                        .cornerRadius(8)
+                }
+                
+            }
+        }
+        .padding()
+        .navigationTitle("Page \(pageIndex + 1)")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 #Preview {
-    StoryPageView()
+    StoryPageView(
+    story: story, pageIndex: 0)
 }
